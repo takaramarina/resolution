e2fb2aa9dfac7ee1c37bbd1d00b15e07a2a622eb
Marina Takara <takara.marina@gmail.com>
2025-04-23 01:19:27 -0500

    changed home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Link from "next/link";
 
 export default function Home() {
   const [showIntro, setShowIntro] = useState(true);
@@ -19,57 +20,27 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-[90vh] mt-[69px]">
       <Header />
 
-      <div className="fixed top-1/2 left-0 right-0 flex flex-col items-center justify-center space-y-4 z-50">
-        <a
-          href="/works"
-          className="text-black text-xl hover:underline hover:decoration-black"
-        >
-          Works
-        </a>
-        <a
-          href="/digital"
-          className="text-black text-xl hover:underline hover:decoration-black"
-        >
-          Digital
-        </a>
-        <a
-          href="/about"
-          className="text-black text-xl hover:underline hover:decoration-black"
-        >
-          About
-        </a>
-        <a
-          href="/contact"
-          className="text-black text-xl hover:underline hover:decoration-black"
-        >
-          Contact
-        </a>
-      </div>
-
-      {/* {showIntro && (
-        <div className={`fixed inset-0 flex flex-col justify-center items-center bg-white z-50 transition-opacity duration-1000 ${fadeOut ? "opacity-0" : "opacity-100"}`}>
-          <h1 className="text-4xl font-bold mb-4 transition-transform duration-1000 transform ${fadeOut ? 'scale-90' : 'scale-110'}">
-            REIJI SHIMANE
-          </h1>
-          <div className="flex items-center space-x-2">
-            <div className="w-3 h-3 bg-black rounded-full animate-ping"></div>
-          </div>
-        </div>
-      )} */}
-
-      <div className="flex-grow overflow-auto flex flex-col items-start pt-[30px]">
-        <div
-          className="w-[1332px] sm:w-[2664px] aspect-[1332/1784] bg-no-repeat bg-top-left bg-contain"
-          style={{
-            backgroundImage: "url('/images/hugedigital.jpg')",
-          }}
+      <div className="flex-grow flex flex-col items-center justify-center text-center space-y-4 px-4">
+        <img
+          src="/images/home.jpg"
+          alt="Digital Artwork"
+          className="h-[40vh] md:h-[60vh] object-contain"
         />
+        <div className="text-sm md:text-base text-gray-700 mt-8">
+          <p><span className="font-medium italic">T</span>, 2025</p>
+        </div>
+        <Link
+          href="/works"
+          className="text-sm md:text-base text-gray-500 hover:text-black transition"
+        >
+          See more works →
+        </Link>
       </div>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
