bbf2cbc7b1a303a7a8553948a5c99f6a529f1c7a
Marina Takara <takara.marina@gmail.com>
2025-04-28 16:18:10 -0500

    consistent styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,23 +4,20 @@ import Footer from "../../components/Footer";
 
 export default function About() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-
-      {/* Main content */}
-      <div className="flex-grow flex flex-col items-center py-16 px-4 sm:px-8 pt-[80px]">
-        {/* <h1 className="text-3xl sm:text-4xl font-bold mb-8">About</h1> */}
+    <div className="min-h-screen bg-white mx-0 md:mx-[60px]">
+          <Header />
+    
+        <main className="px-5 md:px-0">
         <h1 className="text-3xl font-bold text-center mt-10 mb-8 tracking-tight">
           About
         </h1>
-        {/* Introduction section */}
         <section className="w-full max-w-3xl mb-12">
           <p className="text-base sm:text-lg text-gray-700">
             Reiji Shimane (b. 1997)<br /> <br />
             Global artist currently based in Tokyo.<br />
           </p>
         </section>
-      </div>
+      </main>
 
       <Footer />
     </div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,22 +4,19 @@ import Footer from "../../components/Footer";
 
 export default function Contact() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen bg-white mx-0 md:mx-[60px]">
       <Header />
-
-      {/* Main content */}
-      <div className="flex-grow flex flex-col items-center py-16 px-4 sm:px-8 pt-[80px]">
-        {/* <h1 className="text-3xl sm:text-4xl font-bold mb-8">About</h1> */}
-        <h1 className="text-3xl font-bold text-center mt-10 mb-8 tracking-tight">
-          Contact
-        </h1>
-        {/* Introduction section */}
-        <section className="w-full max-w-3xl mb-12">
-          <p className="text-base sm:text-lg text-gray-700">
-            For any inquiries, please email fannymoneyonline@gmail.com.
-          </p>
-        </section>
-      </div>
+    
+      <main className="px-5 md:px-0">
+      <h1 className="text-3xl font-bold text-center mt-10 mb-8 tracking-tight">
+        Contact
+      </h1>
+      <section className="w-full max-w-3xl mb-12">
+        <p className="text-base sm:text-lg text-gray-700">
+          For any inquiries, please email fannymoneyonline@gmail.com.
+        </p>
+      </section>
+      </main>
 
       <Footer />
     </div>
