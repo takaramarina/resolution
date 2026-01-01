3d1e44b69510c9da8765d9d3c7dce719e145b966
Marina Takara <takara.marina@gmail.com>
2026-01-02 00:29:34 +0900

    fix: deleting unnecessary files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,16 +6,19 @@ export default function Footer() {
     <footer className="bg-gray-100 py-8 content-padding" style={{ width: '100vw', marginLeft: 'calc(-50vw + 50%)' }}>
       {/* <div className="border-b border-gray-300 md:mx-[60px] mb-4" /> */}
       <div className="flex flex-col items-left text-sm">
-        <Link href="/works" className="hover:underline">
-        All Works
+        <Link href="/vector" className="hover:underline" style={{ fontFamily: 'GeneralSans-Regular' }}>
+          Vector
         </Link>
-        <Link href="/available-works" className="hover:underline">
-          Available Works
+        <Link href="/raster" className="hover:underline" style={{ fontFamily: 'GeneralSans-Regular' }}>
+          Raster
         </Link>
-        <Link href="/about" className="hover:underline">
+        <Link href="/practice" className="hover:underline" style={{ fontFamily: 'GeneralSans-Regular' }}>
+          Practice
+        </Link>
+        <Link href="/about" className="hover:underline" style={{ fontFamily: 'GeneralSans-Regular' }}>
           About
         </Link>
-        <Link href="/contact" className="mb-4 hover:underline">
+        <Link href="/contact" className="mb-4 hover:underline" style={{ fontFamily: 'GeneralSans-Regular' }}>
           Contact
         </Link>
         
